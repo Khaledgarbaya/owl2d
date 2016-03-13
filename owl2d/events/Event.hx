06c35d47c47f201a53a0d90823921884cfa8c6f8
Khaledgarbaya <khaledgarbaya@gmail.com>
2016-03-13 16:53:28 +0100

    Added Events

---
+++ owl2d/events/Event.hx
@@ -0,0 +1,5 @@
+package owl2d.events;
+class Event {
+    public function new() {
+    }
+}
---
+++ owl2d/events/TouchEvent.hx
@@ -0,0 +1,6 @@
+package owl2d.events;
+class TouchEvent {
+    public static inline var TOUCH: String = "touch";
+    public function new() {
+    }
+}
